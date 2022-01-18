d894babc06c3c0fedb7fcdc4da771c43f73b9f3e
Milena Monova <mimonova13@gmail.com>
2022-01-18 16:13:23 +0200

    updated chainlink versions

--- README.md
+++ README.md
@@ -1,5 +1,135 @@
-# SkillWallet external adapter
+# SkillWallet external adapter for validating offchain signatures
+## Input Params
 
-Skill wallet external adapter, enables endpoints for:
+- `pubKey`, `signature`, `getNonceUrl`, `deleteNonceUrl`
 
-- Verifying SW signature
+## Output
+
+```json
+{
+ "jobRunID": "278c97ffadb54a5bbb93cfec5f7b5503",
+ "data": {
+  "result": true
+ },
+ "statusCode": 200
+}
+```
+
+## Install Locally
+
+Install dependencies:
+
+```bash
+yarn
+```
+
+### Test
+
+Run the local tests:
+
+```bash
+yarn test
+```
+
+Natively run the application (defaults to port 8080):
+
+### Run
+
+```bash
+yarn start
+```
+
+## Call the external adapter/API server
+
+```bash
+curl -X POST -H "content-type:application/json" "http://localhost:8080/" --data '{
+    "jobRunID": "1", 
+    "pubKey": "0442f1fa140d9fcb8ddca188ffc83d1512bcb3be4de464512169c4555f3f7a6ca5e3afb51f7604cdbbf4234b6958852d0b4c57b0ba18af4350a652e889f7f6660a",
+    "signature": "304402207f6139c3fb2772e43c6a43ed1ff7205d29386d44415bb4307455aa87a3362ab4022072a926ba8eedd169760c96911043b538a477089f20044985506fa4d5c20f0bc2",
+    "getNonceUrl": "https://api.skillwallet.id/api/skillwallet/1/nonces?action=0",
+    "deleteNonceUrl": "https://api.skillwallet.id/api/skillwallet/1/nonces?action=0"
+}'
+```
+## Docker
+
+If you wish to use Docker to run the adapter, you can build the image by running the following command:
+
+```bash
+docker build . -t external-adapter
+```
+
+Then run it with:
+
+```bash
+docker run -p 8080:8080 -it external-adapter:latest
+```
+
+## Serverless hosts
+
+After [installing locally](#install-locally):
+
+### Create the zip
+
+```bash
+zip -r external-adapter.zip .
+```
+
+### Install to AWS Lambda
+
+- In Lambda Functions, create function
+- On the Create function page:
+  - Give the function a name
+  - Use Node.js 12.x for the runtime
+  - Choose an existing role or create a new one
+  - Click Create Function
+- Under Function code, select "Upload a .zip file" from the Code entry type drop-down
+- Click Upload and select the `external-adapter.zip` file
+- Handler:
+    - index.handler for REST API Gateways
+    - index.handlerv2 for HTTP API Gateways
+- Add the environment variable (repeat for all environment variables):
+  - Key: API_KEY
+  - Value: Your_API_key
+- Save
+
+#### To Set Up an API Gateway (HTTP API)
+
+If using a HTTP API Gateway, Lambda's built-in Test will fail, but you will be able to externally call the function successfully.
+
+- Click Add Trigger
+- Select API Gateway in Trigger configuration
+- Under API, click Create an API
+- Choose HTTP API
+- Select the security for the API
+- Click Add
+
+#### To Set Up an API Gateway (REST API)
+
+If using a REST API Gateway, you will need to disable the Lambda proxy integration for Lambda-based adapter to function.
+
+- Click Add Trigger
+- Select API Gateway in Trigger configuration
+- Under API, click Create an API
+- Choose REST API
+- Select the security for the API
+- Click Add
+- Click the API Gateway trigger
+- Click the name of the trigger (this is a link, a new window opens)
+- Click Integration Request
+- Uncheck Use Lamba Proxy integration
+- Click OK on the two dialogs
+- Return to your function
+- Remove the API Gateway and Save
+- Click Add Trigger and use the same API Gateway
+- Select the deployment stage and security
+- Click Add
+
+### Install to GCP
+
+- In Functions, create a new function, choose to ZIP upload
+- Click Browse and select the `external-adapter.zip` file
+- Select a Storage Bucket to keep the zip in
+- Function to execute: gcpservice
+- Click More, Add variable (repeat for all environment variables)
+  - NAME: API_KEY
+  - VALUE: Your_API_key
--- validateSignature.js
+++ validateSignature.js
@@ -1,4 +1,4 @@
-const { Validator } = require('@chainlink/external-adapter')
+const { Requester, Validator } = require('@chainlink/external-adapter')
 const { default: axios } = require('axios')
 var eccryptoJS = require('eccrypto-js');
 var EC = require('elliptic').ec;
@@ -9,102 +9,93 @@ var ec = new EC('p256');
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-    pubKey: ['pubKey', 'publicKey'],
-    signature: ['sig', 'signature'],
-    getNonceUrl: ['getNonceUrl', 'getNonce'],
-    deleteNonceUrl: ['deleteNonceUrl', 'delNonceUrl'],
-    endpoint: false
+  pubKey: ['pubKey', 'publicKey'],
+  signature: ['sig', 'signature'],
+  getNonceUrl: ['getNonceUrl', 'getNonce'],
+  deleteNonceUrl: ['deleteNonceUrl', 'delNonceUrl'],
+  endpoint: false
 }
 
 const validateSignature = async (input, callback) => {
-    console.log('chainlink adapter called');
-    // The Validator helps you validate the Chainlink request data
-    const validator = new Validator(callback, input, customParams)
-    const jobRunID = validator.validated.id;
-    const pubKey = validator.validated.data.pubKey;
-    const signature = validator.validated.data.signature;
-    const getNonceUrl = validator.validated.data.getNonceUrl;
-    const deleteNonceUrl = validator.validated.data.deleteNonceUrl;
-    console.log('validation passed');
+  // The Validator helps you validate the Chainlink request data
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id;
+  const pubKey = validator.validated.data.pubKey;
+  const signature = validator.validated.data.signature;
+  const getNonceUrl = validator.validated.data.getNonceUrl;
+  const deleteNonceUrl = validator.validated.data.deleteNonceUrl;
 
-    const noncesResp = await axios.get(getNonceUrl)
-    console.log('fetched nonces');
+  const noncesResp = await axios.get(getNonceUrl)
+  console.log('fetched nonces');
 
+  try {
     const nonces = noncesResp.data.nonces;
     let validNonce = '';
     console.log(nonces);
     let foundValidNonce = false;
     for (const nonce of nonces) {
-        console.log(nonce);
-        const msg = Buffer.from(nonce.toString());
-        const hash = await eccryptoJS.sha256(msg);
-        const public = ec.keyFromPublic(pubKey, 'hex');
-        const isVerified = public.verify(hash, signature)
+      console.log(nonce);
+      const msg = Buffer.from(nonce.toString());
+      const hash = await eccryptoJS.sha256(msg);
+      const public = ec.keyFromPublic(pubKey, 'hex');
+      const isVerified = public.verify(hash, signature)
 
-        if (isVerified) {
-            foundValidNonce = true;
-            validNonce = nonce;
-            console.log('found valid nonce');
-            break;
-        }
-        console.log('did not find valid nonce');
+      if (isVerified) {
+        foundValidNonce = true;
+        validNonce = nonce;
+        console.log('found valid nonce');
+        break;
+      }
+      console.log('did not find valid nonce');
 
     }
     // TODO: fix & uncomment
     if (foundValidNonce && deleteNonceUrl)
-        await axios.delete(deleteNonceUrl + `&nonce=${validNonce}`);
+      await axios.delete(deleteNonceUrl + `&nonce=${validNonce}`);
 
-    const response = {
-        jobRunID: jobRunID,
-        data: foundValidNonce,
-        result: foundValidNonce,
-        statusCode: 200
-    }
+    callback(200, {
+      jobRunID,
+      data: {
+        isValid: foundValidNonce
+      }
+    });
 
-    if(foundValidNonce) { 
-        callback(200, {
-            jobRunID, 
-            data: true, 
-            result: true, 
-            status: 200
-        });
-    } else {
-        callback(400, {
-            jobRunID, 
-            data: false, 
-            result: false, 
-            status: 200
-        });
-    }
-
+  } catch (error) {
+    callback(500, {
+      jobRunID,
+      data: {
+        isValid: false
+      }
+    })
+  };
 }
 
 // This is a wrapper to allow the function to work with
 // GCP Functions
 exports.gcpservice = (req, res) => {
-    validateSignature(req.body, (statusCode, data) => {
-        res.status(statusCode).send(data)
-    })
+  validateSignature(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
 }
 
 // This is a wrapper to allow the function to work with
 // AWS Lambda
 exports.handler = (event, context, callback) => {
-    validateSignature(event, (statusCode, data) => {
-        callback(null, data)
-    })
+  validateSignature(event, (statusCode, data) => {
+    callback(null, data)
+  })
 }
 
 // This is a wrapper to allow the function to work with
 // newer AWS Lambda implementations
 exports.handlerv2 = (event, context, callback) => {
-    validateSignature(JSON.parse(event.body), (statusCode, data) => {
-        callback(null, {
-            statusCode: statusCode,
-            body: JSON.stringify(data),
-            isBase64Encoded: false
-        })
+  validateSignature(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
     })
+  })
 }
 
 // This allows the function to be exported for testing
